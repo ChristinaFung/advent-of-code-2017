e2eeffc4bc2ca6c55e10deb2b12328a4a9247880
Christina Fung <fungc93@gmail.com>
2018-01-01 18:46:16 -0500

    Day 3: part 2 done - first accomplishment of 2018...

--- advent-of-code-2017/src/com/christina/Day3.java
+++ advent-of-code-2017/src/com/christina/Day3.java
@@ -5,12 +5,12 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
-
 import static java.lang.Math.abs;
 
 public class Day3 {
     public static void main(String[] args) throws IOException {
         System.out.println(solvePartOne());
+        System.out.println(solvePartTwo());
     }
 
     public static int solvePartOne() throws IOException {
@@ -58,12 +58,9 @@ public class Day3 {
     }
 
     public static int solvePartTwo() throws IOException {
-        int answer = 0;
-        int sum = 0;
-        int input = 368078;
-        HashMap<Coordinate, Integer> data = new HashMap<>();
-
         /*
+            NOTES:
+
             1. dont know any values beforehand
                 - need dynamic data structure
             2. current value based on prev values
@@ -73,35 +70,98 @@ public class Day3 {
                 - key value pairs (key: coordinate, value: sum at that coord)
 
            next:
-           - how to represent coordinates as keys - DONE
+           - how to represent coordinates as keys
            - how to get surrounding values
-                - (x-1), (y-1), (x+1) (y+1)
            - how to iterate through all the key value pairs
          */
 
+        int sum = 0;
+        int input = 368078;
+        int layer = 1; // represents each "layer" of the spiral pattern
+        HashMap<Coordinate, Integer> data = new HashMap<>();
 
-        while (sum < input) {
-            
+        data.put(new Coordinate(0,0), 1);
+        Coordinate currentCoord = new Coordinate(0,0);
+
+        parentLoop:
+         while (sum < input) {
+             Coordinate topRight = new Coordinate(layer, layer);
+             Coordinate topLeft = new Coordinate(-layer, layer);
+             Coordinate bottomLeft = new Coordinate(-layer, -layer);
+             Coordinate bottomRight = new Coordinate(layer, -layer);
+
+            // go one to the right of origin
+            currentCoord = currentCoord.goRight();
+            sum = getSum(currentCoord, data);
+            data.put(currentCoord, sum);
+
+            if (sum > input) {
+                break parentLoop;
+            }
+
+            // one to the right of origin to top right (lol)
+            while(!currentCoord.equals(topRight)) {
+                currentCoord = currentCoord.goUp();
+                sum = getSum(currentCoord, data);
+                data.put(currentCoord, sum);
+
+                if (sum > input) {
+                    break parentLoop;
+                }
+            }
+
+            // top right to top left
+            while(!currentCoord.equals(topLeft)) {
+                currentCoord = currentCoord.goLeft();
+                sum = getSum(currentCoord, data);
+                data.put(currentCoord, sum);
+
+                if (sum > input) {
+                    break parentLoop;
+                }
+            }
+
+            // top left to bottom left
+            while(!currentCoord.equals(bottomLeft)) {
+                currentCoord = currentCoord.goDown();
+                sum = getSum(currentCoord, data);
+                data.put(currentCoord, sum);
+
+                if (sum > input) {
+                    break parentLoop;
+                }
+            }
+
+            // bottom left to bottom right
+            while(!currentCoord.equals(bottomRight)) {
+                currentCoord = currentCoord.goRight();
+                sum = getSum(currentCoord, data);
+                data.put(currentCoord, sum);
+
+                if (sum > input) {
+                    break parentLoop;
+                }
+            }
+
+            layer++;
         }
 
-
-        return answer;
+        return sum;
     }
 
     public static int getSum(Coordinate coord, HashMap<Coordinate, Integer> hashmap) {
-        int sum = 1;
-        Coordinate points = coord;
+        int sum = 0;
 
-        // coordinates of all points surrounding coord
+        // coordinates of all points surrounding current coord
         Coordinate[] coordinates = {
-                new Coordinate(points.x+1,points.y),
-                new Coordinate(points.x+1,points.y+1),
-                new Coordinate(points.x,points.y+1),
-                new Coordinate(points.x-1,points.y+1),
-                new Coordinate(points.x-1,points.y),
-                new Coordinate(points.x-1,points.y-1),
-                new Coordinate(points.x,points.y-1),
-                new Coordinate(points.x+1,points.y-1),
+                new Coordinate(coord.x+1,coord.y),
+                new Coordinate(coord.x+1,coord.y+1),
+                new Coordinate(coord.x,coord.y+1),
+                new Coordinate(coord.x-1,coord.y+1),
+                new Coordinate(coord.x-1,coord.y),
+                new Coordinate(coord.x-1,coord.y-1),
+                new Coordinate(coord.x,coord.y-1),
+                new Coordinate(coord.x+1,coord.y-1)
         };
 
         List<Coordinate> surroundingVals = Arrays.asList(coordinates);
@@ -110,12 +170,9 @@ public class Day3 {
             Coordinate key = coordinates[i];
 
             if (hashmap.containsKey(key)) {
-                sum = sum + hashmap.get(key);
-            } else {
-                break;
+                sum += hashmap.get(key);
             }
         }
-        System.out.println("sum: "+sum);
 
         return sum;
     }
@@ -129,12 +186,20 @@ public class Day3 {
             this.y = y;
         }
 
-        public int x() {
-            return this.x;
+        public Coordinate goUp() {
+            return new Coordinate(this.x, this.y + 1);
         }
 
-        public int y() {
-            return this.y;
+        public Coordinate goLeft() {
+            return new Coordinate(this.x - 1, this.y);
+        }
+
+        public Coordinate goDown() {
+            return new Coordinate(this.x, this.y - 1);
+        }
+
+        public Coordinate goRight() {
+            return new Coordinate(this.x + 1, this.y);
         }
 
         @Override
@@ -154,6 +219,11 @@ public class Day3 {
             result = 31 * result + x;
             result = 31 * result + y;
             return result;
+        }
+
+        @Override
+        public String toString() {
+            return "x: "+this.x+", y: "+this.y;
         }
     }
 }

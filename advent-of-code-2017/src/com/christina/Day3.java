0cc7d3e9d3ce5d5b9b24edabe977440d7612577d
Christina Fung <fungc93@gmail.com>
2017-12-31 23:34:30 -0500

    Day 3: part 1 done, 28 days later

--- advent-of-code-2017/src/com/christina/Day3.java
+++ advent-of-code-2017/src/com/christina/Day3.java
@@ -1,17 +1,54 @@
 package com.christina;
 
 import java.io.IOException;
+import static java.lang.Math.abs;
 
 public class Day3 {
     public static void main(String[] args) throws IOException {
+        System.out.println(solvePartOne());
+    }
+
+    public static int solvePartOne() throws IOException {
+        // if we know n, we can find the abs value of "y" for the "middle value" in each section of a spiral layer
+        // 1 is at 0,0
+        // the steps = (abs value of x) + (abs value of y)
+        // middle value = corner - (n/2)
+        // y of the middle value = quotient (int value) of (n / 2)
+
+        //finding (n * n) and n
         int n = 1;
         int input = 368078;
+        int steps = 0;
+
+        // corner values
+        int topLeftVal;
+        int topRightVal;
+        int bottomLeftVal;
+        int bottomRightVal;
+        int middleVal = 0;
 
         while ((n * n) < input) {
             n = n + 1;
-            System.out.println("n: "+n);
-            System.out.println("n*n: "+(n * n));
         }
-        System.out.println("n*n: "+(n * n));
+
+        bottomRightVal = (n * n);
+        bottomLeftVal = (n*n) - (n - 1);
+        topLeftVal = (n*n) - ((2*n) - 2);
+        topRightVal = (n*n) - ((3*n) - 3);
+
+        // finding middle ("cross section") values
+        if (input >= bottomLeftVal && input < bottomRightVal) {
+            middleVal = bottomRightVal - (n/2);
+        } else if (input >= topLeftVal && input < bottomLeftVal ) {
+            middleVal = bottomLeftVal - (n/2);
+        } else if (input >= topRightVal && input < topLeftVal) {
+            middleVal =  topLeftVal - (n/2);
+        } else if (input >= (topRightVal - (n-2)) && input < topRightVal) {
+            middleVal = topRightVal - (n/2);
+        }
+
+        steps = abs(input - middleVal) + (n/2);
+
+        return steps;
     }
 }
